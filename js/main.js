4a3ccb8b09dd1901a3a601af031ae917d8c3259b
Igor Onischenko <optimum.flaminis@gmail.com>
2016-06-29 23:35:18 +0300

    sider v0.1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider').bxSlider({
+  nextSelector: '#slider-next',
+  prevSelector: '#slider-prev',
+  nextText: '',
+  prevText: '',
+  minSlides: 3,
+  maxSlides: 4,
+  slideWidth: 218,
+  slideMargin: 15,
+  autoControls:false
+})
